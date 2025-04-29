0de8c2114619ef8df3b2ebdf78cd6ec65d07a906
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-04-29 17:57:02 +0200

    Add search, user, and bell icons to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,32 @@
-import { Menu } from "./Icon"
+import { Bell, Menu, Search, User } from "./Icon"
 
 const Header = ({ handleToggleMenu }: {
     handleToggleMenu: () => void
 }) => {
 
     return (
-        <header className="sticky top-0 flex justify-between items-center z-30 py-1 w-full h-14 text-primary-text">
-            <button className="w-10 h-10 flex sm:hidden justify-center items-center rounded-md bg-primary text-primary-bg hover:bg-secondary duration-100 ease-linear hover:text-primary-bg cursor-pointer" onClick={handleToggleMenu}>
-                <Menu />
-            </button>
-            Header
+        <header className="sticky top-0 max-sm:grid max-sm:grid-cols-1 max-sm:grid-rows-2 max-sm:gap-6 sm:flex justify-between items-center z-30 py-1 w-full sm:h-14 text-primary-text">
+            <div className="max-sm:row-start-2">
+                breadcrumbs
+            </div>
+            <div className="max-sm:row-start-1 flex justify-between gap-4 max-sm:w-full">
+                <div className="relative flex items-center has-[input:focus]:[&>button]:text-secondary has-[input:focus]:[&>button]:hover:text-primary-text not-[has-[input:focus]]:[&>button]:hover:text-secondary">
+                    <button className="absolute left-2.5 duration-300 cursor-pointer">
+                        <Search />
+                    </button>
+                    <input
+                        className="w-full bg-transparent placeholder:text-slate-400 text-sm border text-primary-text border-primary rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-secondary hover:border-slate-300 shadow-sm focus:shadow"
+                        placeholder="UI Kits, Dashboards..."
+                    />
+                </div>
+                <div className="flex items-center justify-between gap-4">
+                    <button className="size-10 flex sm:hidden justify-center items-center rounded-md bg-primary text-primary-bg hover:bg-secondary duration-100 ease-linear hover:text-primary-bg cursor-pointer" onClick={handleToggleMenu}>
+                        <Menu />
+                    </button>
+                    <button className="flex justify-center size-8 items-center rounded-full bg-primary text-primary-bg hover:bg-secondary duration-100 ease-linear hover:text-primary-bg cursor-pointer"><Bell /></button>
+                    <button className="flex justify-center size-8 items-center rounded-full bg-primary text-primary-bg hover:bg-secondary duration-100 ease-linear hover:text-primary-bg cursor-pointer"><User /></button>
+                </div>
+            </div>
         </header>
     )
 }
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -21,3 +21,15 @@ export const Close = ({ strokeColor = 'currentColor' }) => (
 export const ArrowLeft = ({ strokeColor = 'currentColor', className = '' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={strokeColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`${className} icon icon-tabler icons-tabler-outline icon-tabler-chevron-left`}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg>
 )
+
+export const User = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" /><path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" /></svg>
+)
+
+export const Bell = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-bell"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14.235 19c.865 0 1.322 1.024 .745 1.668a3.992 3.992 0 0 1 -2.98 1.332a3.992 3.992 0 0 1 -2.98 -1.332c-.552 -.616 -.158 -1.579 .634 -1.661l.11 -.006h4.471z" /><path d="M12 2c1.358 0 2.506 .903 2.875 2.141l.046 .171l.008 .043a8.013 8.013 0 0 1 4.024 6.069l.028 .287l.019 .289v2.931l.021 .136a3 3 0 0 0 1.143 1.847l.167 .117l.162 .099c.86 .487 .56 1.766 -.377 1.864l-.116 .006h-16c-1.028 0 -1.387 -1.364 -.493 -1.87a3 3 0 0 0 1.472 -2.063l.021 -.143l.001 -2.97a8 8 0 0 1 3.821 -6.454l.248 -.146l.01 -.043a3.003 3.003 0 0 1 2.562 -2.29l.182 -.017l.176 -.004z" /></svg>
+)
+
+export const Search = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+)
