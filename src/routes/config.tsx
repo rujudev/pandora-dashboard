db74ae06dcbabb79b5c4f9114df96d285c44492e
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-04-30 19:19:10 +0200

    Refactor routing configuration to define routes and improve breadcrumb handling

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,14 +1,52 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App.tsx";
+import { Athlete, Dashboard, Training } from "../components/Icon.tsx";
+import Athletes from "../pages/Athletes.tsx";
+import Home from "../pages/Home.tsx";
+import Trainings from "../pages/Trainings.tsx";
 import { CrumbData } from "../types/breadcrumb.types.ts";
+
+export const routes = [
+    {
+        name: "Inicio",
+        path: "/",
+        icon: <Dashboard />,
+        element: <Home />,
+        loader: async () => ({ label: "Inicio" }),
+        handle: {
+            crumb: (crumbData: CrumbData) => ({ label: crumbData?.label }),
+        },
+    },
+    {
+        name: "Atletas",
+        path: "athletes",
+        icon: <Athlete />,
+        element: <Athletes />,
+        loader: async () => ({ label: "Atletas" }),
+        handle: {
+            crumb: (crumbData: CrumbData) => ({ label: crumbData?.label }),
+        },
+    },
+    {
+        name: "Entrenamientos",
+        path: "trainings",
+        icon: <Training />,
+        element: <Trainings />,
+        loader: async () => ({ label: "Entrenamientos" }),
+        handle: {
+            crumb: (crumbData: CrumbData) => ({ label: crumbData?.label }),
+        },
+    },
+];
 
 export const router = createBrowserRouter([
     {
-        path: "/",
         element: <App />,
-        loader: async () => ({ label: "Inicio" }),
-        handle: {
-            crumb: (crumbData: CrumbData) => ({ label: crumbData?.label })
-        },
+        children: routes.map(({ path, element, loader, handle }) => ({
+            path,
+            element,
+            loader,
+            handle
+        }))
     },
 ]);
