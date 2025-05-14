abbe4616103ac3423a191853bf78740152e42bf1
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-14 09:44:36 +0200

    Refactor athlete routes to include training history and profile editing

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App.tsx";
-import AthleteComponent from "../components/Athlete.tsx";
+import AthleteComponent from "../components/athlete/Athlete.tsx";
+import AthleteTraining from "../components/athlete/AthleteTraining.tsx";
+import AthleteTrainingsHistory from "../components/athlete/AthleteTrainingsHistory.tsx";
 import { AthletePageIcon, DashboardPageIcon, TrainingPageIcon } from "../components/Icon.tsx";
 import Athletes from "../pages/Athletes.tsx";
 import Home from "../pages/Home.tsx";
 import Trainings from "../pages/Trainings.tsx";
 import { getAthlete } from "../services/athletes.ts";
+import { getTraining } from "../services/trainings.ts";
 import { CrumbData } from "../types/breadcrumb.types.ts";
 
 export const routes = [
@@ -24,26 +27,72 @@ export const routes = [
     path: "athletes",
     icon: <AthletePageIcon />,
     element: <Athletes />,
-    loader: async () => ({ label: "Atletas" }),
+    loader: async () => ({ label: "Atletas", path: '/athletes' }),
     handle: {
-      crumb: (crumbData: CrumbData) => ({ label: crumbData?.label }),
+      crumb: (crumbData: CrumbData) => ({ label: crumbData?.label, path: crumbData.path }),
     },
     children: [
       {
-        path: ":id/edit",
-        name: "Atleta",
-        element: <AthleteComponent />,
+        path: ":athleteId",
         loader: async ({ params }) => {
-          const id = params.id;
-          const athlete = await getAthlete(id);
+          const athlete = await getAthlete(params.athleteId);
+          const name = athlete ? `${athlete.name} ${athlete.last_name}` : '';
 
-          const athleteName = athlete ? `${athlete.name} ${athlete.last_name}` : '';
-
-          return { label: athleteName, ...athlete }
+          return {
+            label: name,
+            isLast: false
+          };
         },
         handle: {
-          crumb: (crumbData: CrumbData) => ({ label: crumbData?.label })
-        }
+          crumb: (crumbData: CrumbData) => ({ label: crumbData.label, isLast: crumbData.isLast })
+        },
+        children: [
+          {
+            path: "edit",
+            name: "Atleta",
+            element: <AthleteComponent />,
+            loader: async ({ params }) => {
+              const athlete = await getAthlete(params.athleteId);
+
+              const athleteName = athlete ? `${athlete.name} ${athlete.last_name}` : '';
+
+              return { label: athleteName, isLast: true, athlete }
+            },
+            handle: {
+              crumb: (crumbData: CrumbData) => ({ label: 'Editar perfil', isLast: crumbData.isLast })
+            },
+          },
+          {
+            path: "trainings",
+            name: "Atleta",
+            element: <AthleteTrainingsHistory />,
+            loader: async ({ params }) => {
+              const athlete = await getAthlete(params.athleteId);
+
+              const athleteName = athlete ? `${athlete.name} ${athlete.last_name}` : '';
+
+              return { label: athleteName, isLast: true, athlete }
+            },
+            handle: {
+              crumb: (crumbData: CrumbData) => ({ label: 'Historial de entrenamientos', path: `/athletes/${crumbData?.athlete?.id}/trainings`, isLast: false })
+            },
+            children: [
+              {
+                path: ':trainingId/edit',
+                element: <AthleteTraining />,
+                loader: async ({ params }) => {
+                  const athlete = await getAthlete(params.athleteId);
+                  const training = await getTraining(athlete?.trainings.find(athleteTraining => athleteTraining === params.trainingId) ?? '');
+
+                  return { label: training?.name, isLast: true, athlete, training }
+                },
+                handle: {
+                  crumb: (crumbData: CrumbData) => ({ label: crumbData.label })
+                }
+              }
+            ]
+          }
+        ]
       }
     ]
   },
