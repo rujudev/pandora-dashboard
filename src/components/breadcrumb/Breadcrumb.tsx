f770c299f1f253a6c2bb225f3e6ceafcfafec97b
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-14 09:52:12 +0200

    Add Breadcrumb and Breadcrumbs components for in-app navigation

--- src/components/breadcrumb/Breadcrumb.tsx
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { CrumbData } from "../types/breadcrumb.types";
+import { CrumbData } from "../../types/breadcrumb.types";
 
 interface BreadcrumbProps {
     crumb: CrumbData,
@@ -8,7 +8,7 @@ interface BreadcrumbProps {
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ crumb }) => {
     const { label, isLast, path } = crumb;
 
-    return isLast ? (
+    return isLast || !path ? (
         <span className="font-sans text-sm leading-normal font-normal text-primary">{label}</span>
     ) : (
         <Link className="font-sans text-sm leading-normal font-normal text-primary-text hover:text-secondary" to={path}>{label}</Link>
--- src/components/breadcrumb/Breadcrumbs.tsx
+++ src/components/breadcrumb/Breadcrumbs.tsx
@@ -1,38 +1,36 @@
 import { useEffect, useState } from "react";
 import { UIMatch, useMatches } from "react-router";
-import { CrumbData, CrumbHandle } from "../types/breadcrumb.types";
+import { CrumbData, CrumbHandle } from "../../types/breadcrumb.types";
 import Breadcrumb from "./Breadcrumb";
 
 const Breadcrumbs = () => {
     const matches = useMatches() as UIMatch<CrumbData, CrumbHandle>[];
-    const [crumbs, setCrumbs] = useState([{ path: '', isLast: false, label: '' }])
-    const crumbRoutes = matches.filter(route => route.pathname !== '/' || Boolean(route.handle?.crumb));
+    const [crumbs, setCrumbs] = useState<CrumbData[]>([])
+    const crumbRoutes = matches.filter(route => route.pathname !== '/' || Boolean(route?.handle?.crumb));
 
     useEffect(() => {
         setCrumbs(crumbRoutes
+            .filter((route) => route?.data && route.handle)
             .map((route, index) => {
-                const { data, handle, pathname } = route;
+                const { data, handle } = route;
                 const crumbData = handle.crumb(data);
-                const crumbPath = pathname.replace(/\/$/, '');
                 const isLast = index === crumbRoutes.length - 1;
 
                 return ({
-                    path: crumbPath,
-                    isLast,
                     ...crumbData,
+                    isLast: crumbData?.isLast || isLast,
                 })
             }))
     }, [matches])
 
     return (
         <div className="flex flex-col gap-2">
-            <ol className="flex gap-2">{crumbs.map(crumb => (
+            <ol className="flex gap-2">{crumbs.map((crumb: CrumbData) => (
                 <>
                     <li><Breadcrumb crumb={crumb} /></li>
                     {!crumb.isLast && <span className="font-sans text-sm leading-normal font-normal">/</span>}
                 </>
             ))}</ol>
-            {/* <h6 className="antialiased tracking-normal font-sans text-base font-semibold leading-relaxed">{crumbs[crumbs.length - 1].label}</h6> */}
         </div>
     )
 }
