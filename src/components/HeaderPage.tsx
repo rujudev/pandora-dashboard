f4e04adc9c1432bc332ac8c07e27bc81da24fa00
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-14 09:53:09 +0200

    Refactor HeaderPage component to improve layout and add back button functionality

--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router";
+import { ArrowLeft } from "./Icon";
 
 interface HeaderProps {
   children?: React.ReactNode;
   title?: string;
   description?: string | React.ReactElement;
+  hasBackButton?: boolean;
 }
 
 const HeaderPage: React.FC<HeaderProps> = ({
   children,
   title,
   description,
+  hasBackButton = false
 }) => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <header className="flex justify-between items-center">
-        <div>
-          <h1 className="antialiased tracking-normal font-sans text-2xl font-semibold leading-relaxed">
-            {title}
-          </h1>
-          <p className="antialiased tracking-normal font-sans text-base font-normal leading-relaxed">
-            {description}
-          </p>
+      <header className="sticky top-0 bg-primary-content z-10 flex max-sm:flex-col max-sm:gap-10 max-sm:items-start justify-between items-center py-4">
+        <div className="flex gap-10">
+          {hasBackButton && (
+            <button className="flex justify-center items-center rounded-md text-primary hover:text-secondary duration-100 ease-linear cursor-pointer" onClick={() => navigate(-1)} ><ArrowLeft /></button>
+          )}
+          <div className="flex flex-col">
+            <h1 className="antialiased tracking-normal font-sans text-2xl font-semibold leading-relaxed">
+              {title}
+            </h1>
+            <p className="antialiased tracking-normal font-sans text-base font-normal leading-relaxed">
+              {description}
+            </p>
+          </div>
         </div>
         {children}
       </header>
