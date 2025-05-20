f335fda4fd341aacfbdf6d978ff9199de7a0f463
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-20 12:50:16 +0200

    Add edit icon and define motion icon in Icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -93,3 +93,7 @@ export const Save = () => (
 export const Movement = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-stretching-2"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M11 4a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" /><path d="M6.5 21l3.5 -5" /><path d="M5 11l7 -2" /><path d="M16 21l-4 -7v-5l7 -4" /></svg>
 )
+
+export const Edit = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+)
