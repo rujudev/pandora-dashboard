0fd5e1269e8d2f0fcbc492fec7868433e3cc0219
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-20 13:11:00 +0200

    Add Info, Sessions and MuscleMovements components in AthleteTrainingsHistory; update logic to obtain and display training, session and muscle movement data.

--- src/components/athlete/AthleteTrainingsHistory.tsx
+++ src/components/athlete/AthleteTrainingsHistory.tsx
@@ -1,12 +1,74 @@
 import { useEffect, useState } from "react";
 import { Link, Outlet, useLoaderData, useLocation, useMatch, useOutletContext } from "react-router";
-import { getTrainingsByAthlete } from "../../services/trainings";
-import { AthleteOutletContext } from "../../types/athlete-route-outlet.types";
-import { LoaderData } from "../../types/loader-data.types";
-import { Training } from "../../types/training.types";
+import { AthleteOutletContext } from "../../interfaces/athlete-route-outlet.interface";
+import { LoaderData } from "../../interfaces/loader-data.interface";
+import { MuscleMovementWithWeightRef } from "../../interfaces/muscle-movement-weight.interface";
+import { Session } from "../../interfaces/session.interface";
+import { Training } from "../../interfaces/training.interface";
+import { getAthleteTrainings } from "../../services/athletes";
+import { getMuscleMovements } from "../../services/muscle-movements";
+import { getSessions } from '../../services/sessions';
+import { getTraining } from "../../services/trainings";
 import { ViewTraining } from "../Icon";
 import List from "../list/List";
 import ListItem from "../list/ListItem";
+
+const Info = ({ startDate, endDate, trainingNumber }: { startDate: string, endDate: string, trainingNumber: number }) => {
+    return (
+        <div className="flex justify-center flex-col gap-2 list-col-grow">
+            <h3 className="text-base">Entrenamiento {trainingNumber}</h3>
+            <p className="text-sm text-neutral-400">
+                {startDate} - {endDate}
+            </p>
+        </div>
+    )
+}
+
+const Sessions = ({ trainingId }: { trainingId: number }) => {
+    const [sessions, setSessions] = useState<Session[]>([]);
+
+    const getTrainingSessions = async () => {
+        const trainingSessions = await getSessions(trainingId);
+
+        trainingSessions.length > 0 && setSessions(trainingSessions);
+    }
+
+    useEffect(() => {
+        trainingId && getTrainingSessions();
+    }, [trainingId])
+
+    return (
+        <div className="flex items-center gap-1 text-sm">
+            <span className="font-medium">{sessions.length}</span>
+            <span>{sessions.length <= 1 ? 'sesión' : 'sesiones'}</span>
+        </div>
+    )
+}
+
+const MuscleMovements = ({ trainingId }: { trainingId: number }) => {
+    const [muscleMovements, setMuscleMovements] = useState<MuscleMovementWithWeightRef[]>([]);
+
+    const getTrainingMuscleMovements = async () => {
+        const trainingMuscleMovements = await getMuscleMovements(trainingId);
+
+        trainingMuscleMovements.length > 0 && setMuscleMovements(trainingMuscleMovements);
+    }
+
+    useEffect(() => {
+        trainingId && getTrainingMuscleMovements();
+    }, [trainingId])
+
+    return (
+        <div className="flex items-center gap-2">
+            {muscleMovements.map(movement => (
+                <div className="flex gap-2 badge badge-md badge-outline badge-info">
+                    <span>{movement.movement_name}:</span>
+                    <span>{movement.weight_ref} Kg</span>
+                </div>
+            ))}
+        </div>
+    )
+}
 
 const AthleteTrainingsHistory = () => {
     const { pathname } = useLocation();
@@ -16,31 +78,34 @@ const AthleteTrainingsHistory = () => {
     const [trainings, setTrainings] = useState<Training[]>([]);
 
     const getTrainings = async () => {
-        const trainings = await getTrainingsByAthlete(athlete);
+        if (athlete) {
+            const athleteTrainingsRel = await getAthleteTrainings(athlete.id_athlete);
 
-        trainings && trainings.length > 0 && setTrainings(trainings)
+            if (athleteTrainingsRel.length > 0) {
+                const trainingPromises = athleteTrainingsRel.map(async athleteTraining => await getTraining(athleteTraining.id_training))
+                const athleteTrainings = (await Promise.all(trainingPromises)).filter(training => training !== null);
+
+                athleteTrainings.length > 0 && setTrainings(athleteTrainings);
+            }
+        }
     }
 
     useEffect(() => {
-        if (isAthleteTrainingsPage) {
+        if (isAthleteTrainingsPage && athlete) {
             setHeaderConfig({
                 hasBackButton: true,
                 hasActionButton: false,
                 title: 'Historial de entrenamientos',
                 description: (
                     <>
-                        Entrenamientos de <strong>{athlete?.name} {athlete?.last_name}</strong>
+                        Entrenamientos de <strong>{athlete?.first_name} {athlete?.last_name}</strong>
                     </>
                 ),
             })
 
-            athlete && getTrainings()
+            getTrainings()
         }
     }, [pathname, athlete])
-
-    useEffect(() => {
-        console.log(trainings)
-    }, [trainings])
 
     return (
         <section className="flex flex-col gap-5">
@@ -53,38 +118,18 @@ const AthleteTrainingsHistory = () => {
                     <div className="flex w-full">
                         <List>
                             {trainings && trainings.length > 0 ? (
-                                trainings.map(athleteTraining => {
+                                trainings.map((athleteTraining, index) => {
                                     const formatter = new Intl.DateTimeFormat('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' });
-                                    const startDate = new Date(athleteTraining?.start_date || '');
-                                    const endDate = new Date(athleteTraining?.end_date || '');
-
-                                    const formatterStartDate = formatter.format(startDate);
-                                    const formatterEndDate = formatter.format(endDate);
-
-                                    const sessionLength = athleteTraining?.sessions.length ?? 0;
+                                    const startDate = formatter.format(athleteTraining.start_date);
+                                    const endDate = formatter.format(athleteTraining.end_date);
 
                                     return (
                                         <ListItem classes="border border-neutral rounded-xl gap-5">
-                                            <div className="flex justify-center flex-col gap-2 list-col-grow">
-                                                <h3 className="text-base">{athleteTraining?.name}</h3>
-                                                <p className="text-sm text-neutral-400">
-                                                    {formatterStartDate} - {formatterEndDate}
-                                                </p>
-                                            </div>
-                                            <div className="flex items-center gap-1 text-sm">
-                                                <span className="font-medium">{sessionLength}</span>
-                                                <span>{sessionLength < 1 ? 'sesión' : 'sesiones'}</span>
-                                            </div>
-                                            <div className="flex items-center gap-2">
-                                                {athleteTraining?.muscle_movements.map(movement => (
-                                                    <div className="flex gap-2 badge badge-md badge-outline badge-info">
-                                                        <span>{movement.name}:</span>
-                                                        <span>{movement.weight} Kg</span>
-                                                    </div>
-                                                ))}
-                                            </div>
+                                            <Info startDate={startDate} endDate={endDate} trainingNumber={index + 1} />
+                                            <Sessions trainingId={athleteTraining.id_training} />
+                                            <MuscleMovements trainingId={athleteTraining.id_training} />
                                             <div className="flex items-center">
-                                                <Link className="btn btn-primary" to={`${athleteTraining?.id}/edit`}>
+                                                <Link className="btn btn-primary" to={`${athleteTraining?.id_training}/edit`}>
                                                     <ViewTraining /> Editar
                                                 </Link>
                                             </div>
@@ -93,13 +138,12 @@ const AthleteTrainingsHistory = () => {
                                 })
 
                             ) : (
-                                <p>El atleta todavía no tiene entrenamientos asignados</p>
+                                <p>{athlete?.first_name} {athlete?.last_name} no tiene entrenamientos asignados</p>
                             )}
                         </List>
                     </div>
                 </>
             ) : <Outlet context={{ setHeaderConfig }} />}
-
         </section>
     )
 }
