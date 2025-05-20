c734ae50ce203fab011faeea261cd736558ce3db
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-20 13:06:32 +0200

    Add StartEndDate component for selection of start and end dates

---
+++ src/components/training/StartEndDate.tsx
@@ -0,0 +1,35 @@
+import { DateRange } from "react-day-picker"
+import { FieldsetDate } from "../fieldset"
+
+const StartEndDate = ({ startDate, endDate, onChange }: {
+    startDate: string,
+    endDate: string,
+    onChange: (dateKey: "start_date" | "end_date", value: string) => void
+}) => {
+    return (
+        <>
+            <FieldsetDate
+                mode="single"
+                legend="Fecha inicio"
+                placeholder="Selecciona una fecha"
+                selected={new Date(startDate)}
+                onSelect={(value: Date | DateRange | undefined) => {
+                    value && onChange('start_date', value.toString())
+                }}
+                showWeekNumber
+            />
+            <FieldsetDate
+                mode="single"
+                legend="Fecha fin"
+                placeholder="Selecciona una fecha"
+                selected={new Date(endDate)}
+                onSelect={(value: Date | DateRange | undefined) => {
+                    value && onChange('end_date', value.toString())
+                }}
+                showWeekNumber
+            />
+        </>
+    )
+}
+
+export default StartEndDate
