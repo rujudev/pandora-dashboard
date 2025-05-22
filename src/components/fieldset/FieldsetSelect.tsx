fa2e5e5b76482829ba7bbdc5371396782f40f4af
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-05-22 12:23:24 +0200

    Update FieldsetSelect to get additional props and unable options

--- src/components/fieldset/FieldsetSelect.tsx
+++ src/components/fieldset/FieldsetSelect.tsx
@@ -2,7 +2,8 @@ import { ChangeEvent, FC } from "react";
 
 type Option = {
     id: string | number,
-    option: string
+    option: any,
+    disabled?: boolean
 }
 
 type Props = {
@@ -10,23 +11,25 @@ type Props = {
     options: Option[];
     label?: string;
     placeholder?: string;
-    value?: number | string;
+    value?: number | string | undefined;
     full?: boolean;
-    onChange: (e: ChangeEvent<HTMLSelectElement>) => void
+    onChange?: (e: ChangeEvent<HTMLSelectElement>) => void
 }
 
-export const FieldsetSelect: FC<Props> = ({ options, legend, value, placeholder, onChange }) => (
-    <fieldset className="fieldset relative w-full">
-        <legend className="fieldset-legend">{legend}</legend>
-        <select
-            value={value}
-            className="select [::picker(select)]:appearance-[base-select] w-full"
-            onChange={onChange}
-        >
-            <option disabled={true}>{placeholder}</option>
-            {options?.map(({ id, option }) => (
-                <option key={id} value={id}>{option}</option>
-            ))}
-        </select>
-    </fieldset>
-)
+export const FieldsetSelect: FC<Props> = ({ options, legend, value, placeholder, onChange }) => {
+    return (
+        <fieldset className="fieldset relative w-full">
+            <legend className="fieldset-legend">{legend}</legend>
+            <select
+                value={value}
+                className="select [::picker(select)]:appearance-[base-select] w-full"
+                onChange={onChange}
+            >
+                <option value="null" disabled={true}>{placeholder}</option>
+                {options?.map(({ id, option, disabled }) => (
+                    <option key={id} value={id} {...disabled && { disabled }}>{option}</option>
+                ))}
+            </select>
+        </fieldset>
+    )
+}
