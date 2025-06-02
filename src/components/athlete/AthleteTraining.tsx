065c65e87b7442d4361fc9ff5939cafc09c14497
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-06-02 20:49:48 +0200

    Refactor AthleteTraining component to streamline state management and improve header configuration for training editing.

--- src/components/athlete/AthleteTraining.tsx
+++ src/components/athlete/AthleteTraining.tsx
@@ -1,168 +1,64 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { isEqual } from "lodash";
+import { useEffect } from "react";
 import { useLoaderData, useLocation, useMatch, useOutletContext } from "react-router";
-import { AthleteOutletContext } from "../../interfaces/athlete-route-outlet.interface";
-import { FullTrainingPlan } from "../../interfaces/interfaces_compuestas.interface";
+import { useAthleteTraining } from "../../hooks/useAthleteTraining";
+import { AthleteOutletContext } from "../../interfaces/athlete/athlete-route-outlet.interface";
 import { LoaderData } from "../../interfaces/loader-data.interface";
+import Button from "../Button";
 import Card from "../card/Card";
 import CardBody from "../card/CardBody";
+import { Save } from "../Icon";
 import MuscleMovements from "../muscleMovements/MuscleMovements";
-import Sessions from "../sessions/Sessions";
+import Sessions from "../session/Sessions";
 import StartEndDate from "../training/StartEndDate";
 
 const AthleteTraining = () => {
     const { pathname } = useLocation();
-    const { training } = useLoaderData() as LoaderData;
+    const { athlete, training } = useLoaderData() as LoaderData;
+    const { state } = useAthleteTraining();
     const { setHeaderConfig } = useOutletContext<AthleteOutletContext>();
     const isAthleteTrainingEditPage = useMatch('/athletes/:athleteId/trainings/:trainingId/edit');
 
-    const defaultAthleteTraining = {
-        id_athlete: 0,
-        first_name: "",
-        last_name: "",
-        category_weight: 0,
-        birth_day: "",
-        sport: "",
-        team: "",
-        thumbnail: "",
-        id_training: 0,
-        start_date: "01-08-1993",
-        end_date: "01-08-1993",
-        period: "",
-        week_type: "",
-        sessions: [],
-        muscle_movements: []
-
-    }
-
-    const [athleteTraining, setAthleteTraining] = useState<FullTrainingPlan>(defaultAthleteTraining)
-
-    const onHandleChangeText = (e: ChangeEvent<HTMLInputElement>) => {
-        const target = e.target;
-        const { id, value } = target;
-
-        if (!Object.keys(athleteTraining).includes(id)) return;
-
-        setAthleteTraining((prev: FullTrainingPlan) => {
-            return {
-                ...prev,
-                [id]: value
-            };
-        })
-    }
-
-    const onHandleChangeDate = (dateKey: "start_date" | "end_date", value: string) => {
-        setAthleteTraining((prevAthlete: FullTrainingPlan) => {
-            return {
-                ...prevAthlete,
-                [dateKey]: value
-            }
-        })
-    }
-
-    const onHandleChangeMovementWeightRef = (movementId: number, value: number) => {
-        setAthleteTraining((prev: FullTrainingPlan) => ({
-            ...prev,
-            muscle_movements: prev.muscle_movements.map(prevMovement =>
-                prevMovement.id_movement === movementId
-                    ? {
-                        ...prevMovement,
-                        weight_ref: value
-                    } : prevMovement
-            )
-        }))
-    }
-
-    const onHandleDeleteMovement = (movementId: number) => {
-        setAthleteTraining((prev: FullTrainingPlan) => ({
-            ...prev,
-            muscle_movements: prev.muscle_movements.filter(prevMovement => prevMovement.id_movement !== movementId)
-        }))
-    }
-
-    const onHandleChangeDayWeek = (dayWeek: string) => {
-        // console.log(dayWeek);
-    }
-
-    const onHandleChangeSessionDayPeriod = (dayPeriod: string) => {
-        // console.log(dayPeriod)
-    }
-
-    const onHandleChangeSessionMuscleMovement = (sessionId: number, movementId: number, exerciseId: number) => {
-        setAthleteTraining((prev: FullTrainingPlan) => ({
-            ...prev,
-            sessions: prev.sessions.map(session => {
-                if (session.id_session !== sessionId) return session;
-
-                return {
-                    ...session,
-                    exercises: session.exercises.map(exercise => {
-                        if (exercise.id_exercise !== exerciseId) return exercise;
-
-                        return {
-                            ...exercise,
-                            id_movement: movementId
-                        }
-                    })
-                }
-            })
-        }))
-    }
-
     useEffect(() => {
-        training &&
-            setAthleteTraining(training);
-
-        console.log(training);
-    }, [training])
-
-    useEffect(() => {
-        if (isAthleteTrainingEditPage && athleteTraining) {
+        if (isAthleteTrainingEditPage && state) {
             const formatter = new Intl.DateTimeFormat('es-ES', { day: '2-digit', month: 'long', year: 'numeric' });
-            const formatterStartDate = formatter.format(new Date(athleteTraining.start_date));
-            const formatterEndDate = formatter.format(new Date(athleteTraining.end_date));
+            const formatterStartDate = formatter.format(new Date(state.start_date));
+            const formatterEndDate = formatter.format(new Date(state.end_date));
 
             setHeaderConfig({
                 hasBackButton: true,
-                hasActionButton: false,
-                title: (
+                title: <>Entrenamiento de {athlete?.first_name} {athlete?.last_name}</>,
+                description: (
                     <>
-                        Entrenamiento <strong>{formatterStartDate} - {formatterEndDate}</strong>
+                        <small>
+                            <strong>{formatterStartDate} - {formatterEndDate}</strong>
+                        </small>
                     </>
                 ),
+                rightContent: (
+                    <Button text="Guardar" disabled={isEqual(state, training)}>
+                        <Save />
+                    </Button>
+                )
             })
         }
-    }, [pathname, athleteTraining])
+    }, [pathname, state, training]);
 
     return (
         <div className="grid grid-cols-2 w-full gap-5">
             <Card>
                 <CardBody>
-                    <StartEndDate
-                        classes="gap-5 grid grid-cols-2"
-                        startDate={athleteTraining.start_date}
-                        endDate={athleteTraining.end_date}
-                        onChange={onHandleChangeDate}
-                    />
+                    <StartEndDate classes="gap-5 grid grid-cols-2" />
                 </CardBody>
             </Card>
             <Card>
-                <CardBody>
-                    <MuscleMovements
-                        muscleMovements={athleteTraining.muscle_movements}
-                        onChangeWeightRef={onHandleChangeMovementWeightRef}
-                        onDeleteMuscleMovement={onHandleDeleteMovement}
-                    />
+                <CardBody classes="flex flex-col gap-5">
+                    <MuscleMovements />
                 </CardBody>
             </Card>
             <Card>
-                <CardBody>
-                    <Sessions
-                        sessions={athleteTraining.sessions}
-                        muscleMovements={athleteTraining.muscle_movements}
-                        onChangeDayWeek={onHandleChangeDayWeek}
-                        onChangeDayPeriod={onHandleChangeSessionDayPeriod}
-                        onChangeExerciseMuscleMovement={onHandleChangeSessionMuscleMovement}
-                    />
+                <CardBody classes="flex flex-col gap-5">
+                    <Sessions />
                 </CardBody>
             </Card>
         </div>
