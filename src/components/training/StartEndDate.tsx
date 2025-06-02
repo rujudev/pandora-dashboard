32ad4253dd2fc075266d5dfcfdd8d523407cffb4
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-06-02 20:47:02 +0200

    Refactor StartEndDate component to utilize useAthleteTraining hook and streamline date handling

--- src/components/training/StartEndDate.tsx
+++ src/components/training/StartEndDate.tsx
@@ -1,32 +1,25 @@
-import { DateRange } from "react-day-picker"
+import { useAthleteTraining } from "../../hooks/useAthleteTraining"
 import { FieldsetDate } from "../fieldset"
 
-const StartEndDate = ({ startDate, endDate, classes, onChange }: {
-    startDate: string,
-    endDate: string,
-    onChange: (dateKey: "start_date" | "end_date", value: string) => void
-    classes?: string,
-}) => {
+const StartEndDate = ({ classes }: { classes?: string }) => {
+    const { state, setTrainingDate } = useAthleteTraining()
+
     return (
         <div {...classes ? { className: classes } : null}>
             <FieldsetDate
                 mode="single"
                 legend="Fecha inicio"
                 placeholder="Selecciona una fecha"
-                selected={new Date(startDate)}
-                onSelect={(value: Date | DateRange | undefined) => {
-                    value && onChange('start_date', value.toString())
-                }}
+                selected={new Date(state.start_date)}
+                onSelect={date => date && setTrainingDate(date, 'start_date')}
                 showWeekNumber
             />
             <FieldsetDate
                 mode="single"
                 legend="Fecha fin"
                 placeholder="Selecciona una fecha"
-                selected={new Date(endDate)}
-                onSelect={(value: Date | DateRange | undefined) => {
-                    value && onChange('end_date', value.toString())
-                }}
+                selected={new Date(state.end_date)}
+                onSelect={date => date && setTrainingDate(date, 'end_date')}
                 showWeekNumber
             />
         </div>
