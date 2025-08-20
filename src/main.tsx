11e0b439f64cd2aeef4ae60c55b3ec58561c4c5d
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 21:56:13 +0200

    Reorganize providers in main.tsx and remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
-import { DialogProvider } from './context/Dialog.context.tsx'
+import { DialogProvider } from './context/dialog.context.tsx'
+import { HeaderPageProvider } from './context/header-page.context.tsx'
+import { ToastProvider } from './context/toast.context.tsx'
 import './index.css'
 import { router } from './routes/config.tsx'
 
 // Router configuration
 
-
+// TODO: agregar strict mode cuando vaya a producción
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ToastProvider>
     <DialogProvider>
-      <RouterProvider router={router} />
+      <HeaderPageProvider>
+        <RouterProvider router={router} />
+      </HeaderPageProvider>
     </DialogProvider>
-  </StrictMode>
+  </ToastProvider>
 )
