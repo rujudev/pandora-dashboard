119ccdf869eab8e96706a76d895dc7b47ecd8200
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 22:16:29 +0200

    Refactor Athletes component
    Improve imports, state management, and age calculation

--- src/pages/Athletes.tsx
+++ src/pages/Athletes.tsx
@@ -1,23 +1,21 @@
+import { differenceInYears } from "date-fns";
 import { useEffect, useState } from "react";
-import { Link, Outlet, useLocation } from "react-router";
-import HeaderPage, { HeaderProps } from "../components/HeaderPage";
+import { Link, Outlet, useMatch } from "react-router";
+import HeaderPage from "../components/headerPage/HeaderPage";
 import { Plus } from "../components/Icon";
 import AthleteActions from "../components/table/AthleteActions";
 import Table, { Column } from "../components/table/Table";
-import { Athlete } from "../interfaces/athlete.interface";
+import { useBreadcrumbs } from "../context/breadcrumb.context";
+import { initHeaderPage } from "../context/header-page.context";
+import { useHeaderPage } from "../hooks/useHeaderPage";
+import { Athlete } from "../interfaces/athlete/athlete.interface";
 import { getAthletes } from "../services/athletes";
-import { calculateAge } from "../utils/date";
 
 const Athletes = () => {
-    const { pathname } = useLocation();
-    const [headerConfig, setHeaderConfig] = useState<HeaderProps>({
-        hasBackButton: false,
-        title: '',
-        description: '',
-    })
+    const { setHeaderConfig } = useHeaderPage()
+    const { setCrumbs } = useBreadcrumbs()
 
-    const isAthletesPage = pathname === '/athletes';
-
+    const isAthletesPage = useMatch('/athletes');
     const columns: Column[] = [
         {
             field: 'selection', render: () => (
@@ -29,13 +27,11 @@ const Athletes = () => {
         { field: 'first_name', headerName: 'Nombre' },
         { field: 'last_name', headerName: 'Apellidos' },
         {
-            field: 'birth_day', headerName: 'Fecha de nacimiento', render: (athlete: Athlete) => (
-                <span>{athlete.birth_day?.toLocaleDateString()}</span>
-            )
+            field: 'birth_day', headerName: 'Fecha de nacimiento'
         },
         {
             field: 'age', headerName: 'Edad', render: (athlete: Athlete) => (
-                <span>{calculateAge(athlete.birth_day)}</span>
+                <span>{differenceInYears(new Date(), athlete.birth_day)}</span>
             )
         },
         { field: 'sport', headerName: 'Deporte' },
@@ -46,16 +42,33 @@ const Athletes = () => {
         },
     ];
 
-    const [rows, setRows] = useState<Athlete[]>([])
+    const [athletes, setAthletes] = useState<Athlete[]>([])
+
+    const getAthletesData = async () => {
+        const { data: athletesData, error: athletesError } = await getAthletes()
+
+        if (athletesError) throw new Error(athletesError.message)
+
+        let rows = athletesData.map(athlete => ({
+            ...athlete,
+            birth_day: athlete.birth_day
+        }))
+
+        setAthletes(rows)
+    }
 
     useEffect(() => {
-        getAthletes().then(setRows)
-    }, [])
+        !isAthletesPage && setHeaderConfig(initHeaderPage)
+        getAthletesData()
+    }, [isAthletesPage])
 
     useEffect(() => {
+        setCrumbs([
+            { label: 'Atletas', path: '/athletes', isLast: true },
+        ])
+
         isAthletesPage &&
             setHeaderConfig({
-                hasBackButton: false,
                 title: 'Atletas',
                 description: "Gestiona los perfiles y datos de los atletas de tu equipo.",
                 rightContent: (
@@ -65,25 +78,20 @@ const Athletes = () => {
                     </Link>
                 )
             })
-    }, [pathname])
+    }, [isAthletesPage])
 
     return (
         <section className="flex flex-col gap-20">
-            <HeaderPage
-                title={headerConfig.title}
-                description={headerConfig.description}
-                hasBackButton={headerConfig.hasBackButton}
-                rightContent={headerConfig.rightContent}
-            />
+            <HeaderPage />
             <main className="flex flex-col gap-4">
                 {
-                    pathname === '/athletes' ? (
+                    isAthletesPage && athletes.length > 0 ? (
                         <Table
                             classes="max-h-95 overflow-y-auto rounded-box border border-base-content/5 bg-base-100"
-                            rows={rows}
+                            rows={athletes}
                             columns={columns}
                         />
-                    ) : <Outlet context={{ setHeaderConfig }} />
+                    ) : <Outlet />
                 }
             </main>
         </section>
