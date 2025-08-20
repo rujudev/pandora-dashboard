11212acc70c4b87d56b43f9319fb91c78e3ace10
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 22:10:26 +0200

    Remove unused functions and clean up the code in the movement service.

--- src/services/movements.ts
+++ src/services/movements.ts
@@ -1,22 +1,6 @@
 import { PostgrestError } from "@supabase/supabase-js";
 import { supabaseClient } from "../db/config";
-import { MuscleMovementWithWeightRef } from "../interfaces/movement/muscle-movement-weight.interface";
 import { MuscleMovement } from "../interfaces/movement/muscle-movement.interface";
-import muscleMovements from "../mocks/muscle_movements.json";
-import trainingMovements from "../mocks/training_movements.json";
-
-export const getMuscleMovements = async (trainingId: number | string): Promise<MuscleMovementWithWeightRef[]> => {
-    const specificTrainingMuscleMovements = trainingMovements.filter(trainingMovement => trainingMovement.id_training === trainingId);
-
-    return await specificTrainingMuscleMovements.map(trainingMovement => {
-        const movement = muscleMovements.find(movement => movement.id_movement === trainingMovement.id_movement)
-
-        return {
-            ...movement as MuscleMovement,
-            weight_ref: trainingMovement.weight_ref
-        }
-    });
-}
 
 export const getAllMovements = async (): Promise<{ movements: MuscleMovement[], error: PostgrestError | null }> => {
     const { data: dataMovements, error: errorMovements } = await supabaseClient
