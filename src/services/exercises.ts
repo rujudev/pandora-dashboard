9771b493f227a03f25abd28e4617e9557f756b2f
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 22:10:33 +0200

    Refactor saveExercises function to improve handling of exercise updates, inserts, and deletions

--- src/services/exercises.ts
+++ src/services/exercises.ts
@@ -1,29 +1,50 @@
+import { PostgrestResponse } from "@supabase/supabase-js";
+import { supabaseClient } from "../db/config";
 import { ExerciseSession } from "../interfaces/exercise-session.interface";
+import { Exercise } from "../interfaces/exercise/exercise.interface";
 import { IntensityExercise } from "../interfaces/intensity-exercise.interface";
-import { ExerciseWithIntensity, Session } from "../interfaces/interfaces_compuestas.interface";
 import exercisesSession from "../mocks/exercise_session.json";
 import exercises from "../mocks/exercises.json";
 import intensityExercises from "../mocks/intensity_exercise.json";
+import { ExerciseWithMeta } from "../pages/Exercises";
+import { mapToDBTableExercise } from "../utils/exercises";
 
-const groupExercises = (trainingSessions: Session[], sessionsExercises: ExerciseSession[]) => {
-    const grouped = [];
-
-    for (const session of trainingSessions) {
-        const exercises = sessionsExercises
-            .filter(item => item.id_session === session.id_session)
-            .map(item => item.id_exercise);
-
-        grouped.push({
-            id_session: session.id_session,
-            exercises
-        });
-    }
-
-    return grouped;
+export const getExercises = async (): Promise<PostgrestResponse<Exercise>> => {
+    return await supabaseClient.from('exercises').select('*');
 }
 
-export const getExercisesOfTrainingSession = async (trainingId: number | string): Promise<ExerciseWithIntensity[]> => {
+export const saveExercises = async (
+    exercises: ExerciseWithMeta[]
+): Promise<[PostgrestResponse<Exercise[]>, PostgrestResponse<Exercise[]>, PostgrestResponse<Exercise[]>]> => {
+    const exercisesToUpdate = exercises.filter(ex => ex.isEditing).map(ex => mapToDBTableExercise(ex));
+    const exercisesToInsert = exercises
+        .filter(ex => ex.isNew)
+        .map(ex => {
+            const { id_exercise, ...exercise } = mapToDBTableExercise(ex);
 
+            return exercise
+        });
+    const exercisesToDelete = exercises.filter(ex => ex.isRemoved).map(ex => mapToDBTableExercise(ex));
+
+    const promises = [
+        exercisesToUpdate.length > 0
+            ? supabaseClient.from('exercises').upsert(exercisesToUpdate, { onConflict: 'id_exercise' }).select('*')
+            : Promise.resolve({} as PostgrestResponse<Exercise[]>),
+        exercisesToInsert.length > 0
+            ? supabaseClient.from('exercises').insert(exercisesToInsert).select('*')
+            : Promise.resolve({} as PostgrestResponse<Exercise[]>),
+        exercisesToDelete.length > 0
+            ? supabaseClient.from('exercises').delete().in('id_exercise', exercisesToDelete.map(ex => ex.id_exercise)).select('*')
+            : Promise.resolve({} as PostgrestResponse<Exercise[]>)
+    ];
+
+    const [updateResponse, insertResponse, deleteResponse] = await Promise.all(promises);
+
+    if (updateResponse?.error || insertResponse.error || deleteResponse.error) {
+        throw new Error(updateResponse.error?.message || insertResponse.error?.message || deleteResponse.error?.message);
+    }
+
+    return [updateResponse, insertResponse, deleteResponse]
 }
 
 export const getExerciseSeriesRepetitions = async (exerciseId: number | string): Promise<IntensityExercise[] | null> => {
