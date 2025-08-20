f17d8f6689a2e97d7e1ba5f3ebea08ba87fd5b82
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 22:09:11 +0200

    Refactor date utility functions to improve date formatting and handling of day periods and weeks

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,5 @@
+import { DayPeriod, DayWeek } from "../types/day.types";
+
 export const calculateAge = (date: Date | null): string => {
     if (!date) return '0';
 
@@ -19,7 +21,56 @@ export const calculateAge = (date: Date | null): string => {
     return age.toString();
 };
 
-export const getFormattedDate = (date: Date, locale: Intl.LocalesArgument, options: Intl.DateTimeFormatOptions) => {
-    const formatter = new Intl.DateTimeFormat(locale, options);
-    return formatter.format(date);
-} 
+export const getFormattedDate = (date: string, locale: Intl.LocalesArgument, options: Intl.DateTimeFormatOptions) => {
+    const newDate = new Date(date)
+    const formatter = new Intl.DateTimeFormat(locale, { day: '2-digit', year: 'numeric', ...options, });
+    return formatter.format(newDate);
+}
+
+export const dayWeekToNumber = (day: DayWeek): number => {
+    const map: Record<DayWeek, number> = {
+        'Lunes': 1,
+        'Martes': 2,
+        'Miércoles': 3,
+        'Jueves': 4,
+        'Viernes': 5,
+        'Sábado': 6,
+        'Domingo': 0
+    };
+
+    return map[day] || 0;
+}
+
+export const numberToDayWeek = (number: number): DayWeek => {
+    const map: Record<number, DayWeek> = {
+        1: 'Lunes',
+        2: 'Martes',
+        3: 'Miércoles',
+        4: 'Jueves',
+        5: 'Viernes',
+        6: 'Sábado',
+        0: 'Domingo'
+    };
+
+    return map[number] || 'Lunes';
+}
+
+export const dayPeriodToNumber = (period: DayPeriod): number => {
+    const map: Record<DayPeriod, number> = {
+        'Mañana': 1,
+        'Tarde': 2,
+        'Noche': 3
+    };
+
+    return map[period] || 1; // Por defecto: Mañana
+};
+
+export const numberToDayPeriod = (number: number): DayPeriod => {
+    const map: Record<number, DayPeriod> = {
+        1: 'Mañana',
+        2: 'Tarde',
+        3: 'Noche'
+    };
+
+    return map[number] || 'Mañana'; // Por defecto
+};
