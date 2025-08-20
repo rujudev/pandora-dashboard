a0c73c9f64fba6c0927446bda6c757913d45bef9
Rubén Juan Molina <rubenjuanmolinawd@gmail.com>
2025-08-20 22:10:52 +0200

    Refactor Collapse component to add onRemove functionality and improve layout structure

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react"
+import { Trash } from "./Icon"
 
 type Props = {
     title?: string,
@@ -6,19 +7,35 @@ type Props = {
     isSubCollapse?: boolean,
     children?: ReactNode,
     name?: string,
-    classes?: string
+    classes?: string,
+    id?: number,
+    onRemove?: (exerciseId: number) => void
 }
 
-const Collapse: FC<Props> = ({ classes, title, titleIcon, isSubCollapse = false, name, children }) => {
+const Collapse: FC<Props> = ({ id, classes, title, titleIcon, isSubCollapse = false, name, children, onRemove = null }) => {
     return (
-        <div tabIndex={0} className={`collapse collapse-arrow${classes ? ` ${classes}` : ''}`}>
-            <input className="pl-12" type="checkbox" name={name} />
-            <div className={`border-neutral rounded-xl ${isSubCollapse ? `pl-4` : "after:left-[1.4rem] pl-12"} flex gap-5 collapse-title font-semibold`}>
-                {titleIcon && titleIcon}
-                <h1 className="text-lg">{title}</h1>
+        <>
+            <div tabIndex={0} className={`collapse grid-cols-12 collapse-arrow${classes ? ` ${classes}` : ''}`}>
+                <input className={`pl-12 ${onRemove ? ' !col-start-2 col-span-12' : ''}`} type="checkbox" name={name} />
+                {onRemove && (
+                    <div className="flex justify-center items-center">
+                        <button
+                            className="col-start-1 row-start-1 duration:100 transition-colors hover:text-error cursor-pointer"
+                            onClick={() => onRemove(id || 0)}
+                        >
+                            <Trash />
+                        </button>
+                    </div>
+                )}
+                <div className={`${onRemove ? 'col-start-2 col-span-12 flex gap-5 ' : ''}border-neutral rounded-xl ${isSubCollapse ? `pl-4` : "after:left-[1.4rem] pl-12"} collapse-title font-semibold`}>
+                    {titleIcon && titleIcon}
+                    <h1 className="text-lg">{title}</h1>
+                </div>
+                <div className="flex flex-col gap-5 collapse-content text-sm col-span-12">
+                    {children}
+                </div>
             </div>
-            {children}
-        </div >
+        </>
     )
 }
 
